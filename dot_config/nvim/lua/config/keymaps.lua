2d0ad98d5adf8a1c7b02fa2a4e2883987975b611
jlewsader <jlewsader@gmail.com>
2026-08-29 23:36:24 -0400

    Fix iPad normal mode keys to add visual mode as well

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,8 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- "jk" save for iPad keyboard
+-- "jk" return to Normal mode for iPad keyboard
 vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("v", "jk", "<Esc>")
 
 -- Windows Keys :)
 -- vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<Esc>:w<CR>", { silent = true })
